87669330abf2ccbb183137414863d01fb01272b2
Daniel Aucatoma <d.aucatoma96@gmail.com>
2020-08-05 21:26:42 -0500

    Avoid some errors while building app

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,10 +3,6 @@ import React from "react";
 
 export class Home extends React.Component {
 
-    constructor(props: any) {
-        super(props);
-    }
-
     render() {
         return (
             "Hello to our notes app"
